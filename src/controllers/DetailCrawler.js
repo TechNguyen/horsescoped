4d9a4408469f6be3faa80ea739c4ce12fe9a4dd1
TechNguyen <ndt13102003@gmail.com>
2025-02-08 00:57:51 +0700

    1

---
+++ src/controllers/DetailCrawler.js
@@ -0,0 +1,26 @@
+const asyncHandler = require("express-async-handler");
+
+const {
+    getPageList,
+    createOne,
+} = require("../core/controllers/baseController");
+const DetailCrawerl = require("../models/DetailCrawerl");
+const ApiResponse = require("../utils/apiResponse");
+const CreateDetail = asyncHandler(async (req, res) => {
+    try {
+        const result = await getPageList(DetailCrawerl, req.params);
+        if (result.getStatus()) {
+            return result.createResSuccess(res);
+        } else {
+            return result.createResError(res);
+        }
+    } catch (err) {
+        return res
+            .status(400)
+            .json(new ApiResponse(500, "Internal server", err, false));
+    }
+});
+
+module.exports = {
+    CreateDetail,
+};
--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -74,7 +74,7 @@ const authorize = (allowedRoles = []) => {
                                 false,
                             ),
                         );
-                }
+           s     }
 
                 req.user = user;
             }
